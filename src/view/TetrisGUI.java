ef1bbab285bdc550fa31cae50762de047c62974f
Tyler <tylerc99@uw.edy>
2023-11-20 13:18:40 -0800

    Created very basic TetrisGUI panel to test Menu. Implemented action for file (game) menu and about menu.

---
+++ src/view/TetrisGUI.java
@@ -0,0 +1,20 @@
+package view;
+
+import javax.swing.*;
+
+public class TetrisGUI extends JFrame {
+
+    public TetrisGUI() {
+        setJMenuBar(new Menu());
+
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Center on screen
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // Ensure GUI is created in the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> new view.TetrisGUI());
+    }
+}
