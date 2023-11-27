c297982fe8c001e064587cdecc4c9966211e443d
Tyler <tylerc99@uw.edy>
2023-11-27 05:20:42 -0800

    Refactor TetrisGUI for Enhanced OOP and Code Readability
    - Introduced instance variables myBoard and gameTimer to manage the game board and timing events, replacing any static method usage.
    - Implemented setUpComponents method to organize GUI initialization, including setting up the main frame, menu bar, and game panel.
    - Added addListeners method to centralize the addition of action listeners for gameTimer and property change listener for myBoard.
    - Created setupGameTimer method for initializing and configuring the gameTimer.
    - Defined constants BORDER_SIZE_HEIGHT and BORDER_SIZE_WIDTH to replace magic numbers for border dimensions, enhancing code clarity.

--- src/view/Controller/TetrisGUI.java
+++ src/view/Controller/TetrisGUI.java
@@ -1,57 +1,128 @@
 package view.Controller;
 
+import static model.Board.PROPERTY_GAME_OVER;
 import view.Layout.MainPanel;
 import view.Menu.Menu;
-
+import model.Board;
+import model.MyBoard;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class represents the graphical user interface for the Tetris game.
+ * It handles the initialization and display of the game window, including setting up
+ * the game board, menu, timer, and various GUI components.
+ *
+ * @author Group 7
+ * @version 1.0.2
+ *
+ */
 public class TetrisGUI {
 
-    public static void main(String[] args) {
+    /**
+     * The primary model object representing the Tetris game board.
+     */
+    private final MyBoard myBoard;
 
-        final JLabel nextPieceLabel = new JLabel();
-        nextPieceLabel.setText("Next Piece");
+    /**
+     * Timer to manage game updates at regular intervals.
+     */
+    private Timer myGameTimer;
 
-        final JLabel actionsLabel = new JLabel();
-        actionsLabel.setText("User actions");
+    /**
+     * The height of the border around the game window.
+     */
+    private static final int BORDER_SIZE_HEIGHT = 10;
 
-        final JLabel mainBoardLabel = new JLabel();
-        mainBoardLabel.setText("Main Tetris Board");
+    /**
+     * The width of the border around the game window.
+     */
+    private static final int BORDER_SIZE_WIDTH = 10;
 
-        final JLabel scoreLabel = new JLabel();
-        scoreLabel.setText("Score");
+    /**
+     * The number of milliseconds in one second.
+     * This constant defines the interval for the game's timer tick.
+     */
+    private static final int MILLIS_PER_SEC = 1000;
 
+    /**
+     * Constructs a new TetrisGUI object.
+     * Initializes the game board, sets up GUI components, and adds necessary listeners.
+     */
+    public TetrisGUI() {
+        super();
+        myBoard = new Board();
+        setUpComponents();
+        addListeners();
+    }
+
+    /**
+     * Sets up the main components of the GUI.
+     * Initializes the frame, sets up the layout, adds a menu bar, main panel, and borders.
+     */
+    private void setUpComponents() {
         final JFrame frame = new JFrame("Tetris");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        final view.Menu.Menu menuBar = new Menu();
-        frame.setJMenuBar(menuBar); // Here we add the menu bar to the frame
+        frame.setJMenuBar(new Menu());
+        frame.add(new MainPanel(), BorderLayout.CENTER);
+        addBorders(frame);
 
-        //this is the main panel which contains all the other panels.
-        final JPanel main = new MainPanel();
-        frame.add(main, BorderLayout.CENTER);
-        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null); // centers GUI on screen when ran
+        setupGameTimer();
+        frame.setVisible(true);
+    }
 
-        //these are blank panels used to create an outer border.
+    /**
+     * Adds action listeners to components.
+     * This includes the game timer and property change listener for the game board.
+     */
+    private void addListeners() {
+        myGameTimer.addActionListener(e -> myBoard.step());
+
+        myBoard.addPropertyChangeListener(evt -> {
+            if (PROPERTY_GAME_OVER.equals(evt.getPropertyName()) && (Boolean) evt.getNewValue()) {
+                myGameTimer.stop();
+            }
+        });
+    }
+
+    /**
+     * Sets up the game timer.
+     * Initializes the timer to trigger every second.
+     */
+    private void setupGameTimer() {
+        myGameTimer = new Timer(MILLIS_PER_SEC, null);
+    }
+
+    /**
+     * Adds border panels to the frame.
+     * @param theFrame The JFrame to which the borders are added.
+     */
+    private void addBorders(final JFrame theFrame) {
         final JPanel left = new JPanel();
         final JPanel right = new JPanel();
         final JPanel top = new JPanel();
         final JPanel bottom = new JPanel();
 
-        left.setPreferredSize(new Dimension(10, 10));
-        right.setPreferredSize(new Dimension(10, 10));
-        top.setPreferredSize(new Dimension(10, 10));
-        bottom.setPreferredSize(new Dimension(10, 10));
+        left.setPreferredSize(new Dimension(BORDER_SIZE_WIDTH, BORDER_SIZE_HEIGHT));
+        right.setPreferredSize(new Dimension(BORDER_SIZE_WIDTH, BORDER_SIZE_HEIGHT));
+        top.setPreferredSize(new Dimension(BORDER_SIZE_WIDTH, BORDER_SIZE_HEIGHT));
+        bottom.setPreferredSize(new Dimension(BORDER_SIZE_WIDTH, BORDER_SIZE_HEIGHT));
 
-        frame.add(left, BorderLayout.WEST);
-        frame.add(right, BorderLayout.EAST);
-        frame.add(top, BorderLayout.NORTH);
-        frame.add(bottom, BorderLayout.SOUTH);
-
-        frame.pack(); //this needs to be called after adding all components to the frame
-        frame.setVisible(true); //this needs to be called after adding all components to the frame
+        theFrame.add(left, BorderLayout.WEST);
+        theFrame.add(right, BorderLayout.EAST);
+        theFrame.add(top, BorderLayout.NORTH);
+        theFrame.add(bottom, BorderLayout.SOUTH);
     }
 
+    /**
+     * The main method to run the Tetris GUI.
+     * @param theArgs Command line arguments (not used).
+     */
+    public static void main(final String[] theArgs) {
+        SwingUtilities.invokeLater(TetrisGUI::new);
+    }
 }
