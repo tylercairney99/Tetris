0782d71269f001fb76279346e9b6db80623e159b
Jimothy <skittlejs33@gmail.com>
2023-12-05 17:20:34 -0800

    Fixed checkstyle warnings and imports. Added changing score to ScorePanel(not yet testable). Changed formatting to score text.

--- src/view/layout/ScorePanel.java
+++ src/view/layout/ScorePanel.java
@@ -1,33 +1,68 @@
 package view.layout;
 
-import javax.swing.*;
-import java.awt.*;
+import static model.Board.PROPERTY_ROW_CLEARED;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.JPanel;
+
 
 /**
- * Score panel shows the score of the game.
+ * Panel to display player's score.
+ * Implements PCL to update score.
+ *
+ * @author James
+ * @author Tyler
+ * @author Josh
+ * @author Cam
+ * @version 3.0
  */
-public final class ScorePanel extends JPanel {
-    /**
-     * static int shared amongst all score panel objects to ensure that
-     * there is only ever one instance.
-     */
-    private static int count;
-    /**
-     * The number of rows in the score panel.
-     */
-    private static final int ROWS = 7;
-    /**
-     * The number of columns in the score panel.
-     */
-    private static final int COLS = 2;
+public final class ScorePanel extends JPanel implements PropertyChangeListener {
 
     /**
-     * Constructs a ScorePanel object.
+     * x coord of score text.
+     */
+    private static final int TEXT_X = 15;
+
+    /**
+     * y coord of score text.
+     */
+    private static final int TEXT_Y = 55;
+
+    /**
+     * Size of score text.
+     */
+    private static final int TEXT_SIZE = 20;
+
+    /**
+     * Score gained for each row cleared.
+     */
+    private static final int SCORE_GAIN = 10;
+
+    /**
+     * Ensures only one panel is instantiated.
+     */
+    private static int count;
+
+    /**
+     * Score of the player. Updates when rows are cleared.
+     */
+    private int myScore;
+
+    /**
+     * Panel used to display the player's score.
+     * Sets background color.
      *
      * @throws IllegalArgumentException if more than one ScorePanel is instantiated.
      */
     public ScorePanel() {
         super();
+        myScore = 0;
 
         if (count > 0) {
             throw new IllegalArgumentException("Only one ScorePanel allowed");
@@ -36,11 +71,31 @@ public final class ScorePanel extends JPanel {
 
         setBackground(Color.GREEN);
         setLayout(new BorderLayout());
-
-        final JPanel scorePanel = new JPanel(new GridLayout(ROWS,COLS));
-
+        final JPanel scorePanel = new JPanel(new FlowLayout());
         scorePanel.setOpaque(false);
-        scorePanel.add(new JLabel("Score:" + 0));
-        add(scorePanel, BorderLayout.PAGE_START);
+    }
+
+    @Override
+    public void paintComponent(final Graphics theGraphics) {
+        super.paintComponent(theGraphics);
+        theGraphics.setFont(new Font("" + theGraphics.getFont(), Font.PLAIN, TEXT_SIZE));
+        showScore(theGraphics);
+    }
+
+    /**
+     * Displays player's score in score panel.
+     *
+     * @param theGraphics Graphics object used to display text.
+     */
+    private void showScore(final Graphics theGraphics) {
+        theGraphics.drawString("Score: " + myScore, TEXT_X, TEXT_Y);
+    }
+
+    @Override
+    public void propertyChange(final PropertyChangeEvent theEvent) {
+        if (PROPERTY_ROW_CLEARED.equals(theEvent.getPropertyName())) {
+            myScore += SCORE_GAIN; //multiply by rows cleared.
+            repaint();
+        }
     }
 }
